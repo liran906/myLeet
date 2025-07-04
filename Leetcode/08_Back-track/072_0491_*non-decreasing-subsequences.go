41a15485f38a9aca879c47d68029d5e231f487a8
liran906 <liran900620@gmail.com>
2025-07-04 11:48:01 +0800

    lc

--- Leetcode/08_Back-track/072_0491_*non-decreasing-subsequences.go
+++ Leetcode/08_Back-track/072_0491_*non-decreasing-subsequences.go
@@ -50,3 +50,46 @@ func findSubsequences(nums []int) (ans [][]int) {
 
 	return
 }
+
+// 小优化：用数组代替 map
+func findSubsequences_2(nums []int) (ans [][]int) {
+	var path []int
+
+	// 回溯函数：index 是当前搜索起点，last 是上一个选择的值
+	var bt func(int, int)
+	bt = func(index, last int) {
+		// 当 path 中元素个数 ≥ 2 时，即为合法递增子序列，加入结果集
+		if len(path) > 1 {
+			ans = append(ans, append([]int{}, path...))
+		}
+
+		// 创建数组代替哈希集合，记录本层使用过的数字，避免重复分支
+		// 题目已说明 nums[i] ∈ [-100, 100]，所以 +100 映射到 [0,200]
+		// 虽然都是常数级复杂度，但比用 map 更快更高效（没有 hash 计算、重复、扩容）
+		seen := make([]int, 201)
+
+		for i := index; i < len(nums); i++ {
+			// 不是递增（非严格递增也可以，允许相等）
+			if nums[i] < last {
+				continue
+			}
+
+			// 当前层已经使用过 nums[i]，避免重复选择
+			if seen[nums[i]+100] == 1 {
+				continue
+			}
+
+			// 标记当前层已使用
+			seen[nums[i]+100] = 1
+
+			// 回溯
+			path = append(path, nums[i])
+			bt(i+1, nums[i])
+			path = path[:len(path)-1]
+		}
+	}
+
+	// 初始化 last 为最小整数，以便首次 nums[i] < last 不会发生
+	bt(0, math.MinInt)
+	return
+}
