078d05897df315218c3324b85853e6b509cf3d41
liran906 <liran900620@gmail.com>
2025-08-05 17:16:15 +0800

    lc

---
+++ Leetcode/00_hot150/0088e_合并两个有序数组.go
@@ -0,0 +1,23 @@
+package hot150
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j := m-1, n-1
+	k := m + n - 1
+
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	// 如果 nums2 还有剩余，拷贝到 nums1
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+}
